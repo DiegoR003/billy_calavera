c236b7263d782b92199ddcfce8f0131b4c45521a
DiegoR003 <168403198+DiegoR003@users.noreply.github.com>
2026-02-26 11:36:06 -0700

    distrubución de css, js y configuraciones tailwind

---
+++ tailwind.config.js
@@ -0,0 +1,18 @@
+tailwind.config = {
+            theme: {
+                extend: {
+                    colors: {
+                        // Cambia estos HEX por los de la identidad de la banda
+                        banda: {
+                            dark: '#0a0a0a',
+                            light: '#f3f4f6',
+                            primary: '#dc2626', // Rojo de acento
+                        }
+                    },
+                    fontFamily: {
+                        display: ['Oswald', 'sans-serif'],
+                        body: ['Inter', 'sans-serif'],
+                    }
+                }
+            }
+        }
