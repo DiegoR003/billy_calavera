59412ca8c83e01e681f1de80bedfd24a73863846
DiegoR003 <168403198+DiegoR003@users.noreply.github.com>
2026-03-08 16:59:18 -0700

    cambios de paleta de colores e implementación de animaciones

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,21 +1,20 @@
-
 tailwind.config = {
-            theme: {
-                extend: {
-                    colors: {
-                        // Cambia estos HEX por los de la identidad de la banda
-                        banda: {
-                            dark: '#0a0a0a',
-                            light: '#f3f4f6',
-                            primary: '#dc2626', // Rojo de acento
-                        }
-                    },
-                    fontFamily: {
-                        display: ['Oswald', 'sans-serif'],
-                        body: ['Inter', 'sans-serif'],
-                    }
+    theme: {
+        extend: {
+            colors: {
+                banda: {
+                    dark: '#0a0a0a',      // Mantenemos un fondo oscuro base para el contraste
+                    light: '#d9e0e0',     //  nuevo color hueso/gris claro para textos generales
+                    primary: '#cf9e28',   // El mostaza/dorado para botones, iconos y textos a resaltar
+                    secondary: '#8e5110', // El marrón para efectos hover (pasar el mouse) o bordes
+                    blueLight: '#aac2cc', // Azul claro para detalles secundarios
+                    blueDark: '#86a7b4',  // Azul medio para fondos de secciones alternativas
                 }
+            },
+            fontFamily: {
+                display: ['Oswald', 'sans-serif'],
+                body: ['Inter', 'sans-serif'],
             }
         }
-
-      
+    }
+}
